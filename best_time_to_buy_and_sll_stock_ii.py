e742c51d8b1329fdcf081d3dd7a925a3c84efe6f
mrmoustachie <gab.iom89@gmail.com>
2022-02-18 13:45:27 +0100

    Update best_time_to_buy_and_sll_stock_ii.py

--- best_time_to_buy_and_sll_stock_ii.py
+++ best_time_to_buy_and_sll_stock_ii.py
@@ -1,6 +1,13 @@
 # Best Time to Buy and Sell Stock II
 # https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
 
+"""
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
+However, you can buy it then immediately sell it on the same day.
+Find and return the maximum profit you can achieve.
+"""
+
 class Solution(object):
     def maxProfit(self, prices):
         profit = 0
