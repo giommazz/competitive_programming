0336c8eeb9fda643e39c8212f4dd3c4627464c6c
giommazz <gab.iom89@gmail.com>
2025-08-04 12:32:11 +0200

    update binsearch

--- binary_search.py
+++ binary_search.py
@@ -20,7 +20,40 @@ def _binary_search(nums: list[int], left: int, right: int, target: int) -> int:
         # search right half
         return _binary_search(nums, mid + 1, right, target)
     
-# solution from the website, not using recursion
+
+# returning leftmost found element
+def binarysearch_left(nums, elem):
+    n = len(nums)
+    if n == 1:
+        return 0
+    
+    mid = (n-1) // 2 
+
+    if elem <= nums[mid]:
+        # look left: nums[0] ... nums[mid-1]
+        return binarysearch_left(nums[:mid+1], elem)
+    else:
+        # look right: nums[mid] ... nums[n]
+        return mid + 1 + binarysearch_left(nums[mid+1:], elem)
+
+
+ # Two other solutions of mine: leftmost and rightmost binary search
+# returning rightmost found element
+def binarysearch_right(nums, elem):
+    n = len(nums)
+    if n == 1:
+        return 0
+    
+    mid = n // 2 
+
+    if elem < nums[mid]:
+        # look left: nums[0] ... nums[mid-1]
+        return binarysearch_right(nums[:mid], elem)
+    else:
+        # look right: nums[mid] ... nums[n]
+        return mid + binarysearch_right(nums[mid:], elem)
+
+# solution from the neetcode, not using recursion
 def search(nums: list[int], target: int) -> int:
     l, r = 0, len(nums) - 1
 
